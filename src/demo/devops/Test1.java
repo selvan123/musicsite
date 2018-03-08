98089f9edfefdc23f3db8cf0451a240e8502e511
selvan123 <selvans@gmail.com>
2018-03-08 18:08:07 +0530

    Update Test1.java

--- src/demo/devops/Test1.java
+++ src/demo/devops/Test1.java
@@ -9,13 +9,15 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Test1 {
 	public static WebDriver driver;
 	@BeforeSuite
 	public static void setup(){
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Administrator\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		
+		ChromeOptions options = new ChromeOptions();
+		options.setBinary("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
