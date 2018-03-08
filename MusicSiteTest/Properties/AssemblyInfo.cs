587239eacacbc96c7b2c04eefce3487b8efbf413
SELS <SELS@LIN35006974>
2018-03-08 15:38:23 +0530

    second

---
+++ MusicSiteTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MusicSiteTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Capgemini")]
+[assembly: AssemblyProduct("MusicSiteTest")]
+[assembly: AssemblyCopyright("Copyright © Capgemini 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8f9a4a4e-4c0b-4ddc-89d8-63cc02195800")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MusicSiteTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogicLayer;
+
+namespace MusicSiteTest
+{
+    [TestClass]
+    public class Index
+    {
+        [TestMethod]
+        public void bindTopDownloads()
+        {
+            clsCommonHelper objcommon;
+            Index index = new Index();
+            objcommon = new clsCommonHelper();
+            Assert.IsNotNull(objcommon);
+        }
+    }
+}
